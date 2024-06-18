4d7baad9e6ade1f21a08d677b90072a119bc38c9
gf22\User <sbsgameas@gmail.com>
2024-06-18 21:54:40 +0900

    fgh

--- Assets/script/SceneMaker.cs
+++ Assets/script/SceneMaker.cs
@@ -5,9 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class SceneMaker : MonoBehaviour
 {
+    [SerializeField] Scene testsc;
+    [SerializeField] Vector2 testsc2;
+
     void Start()
     {
         Scene NewScene = SceneManager.CreateScene("Test");
+        //Scene testScene = SceneManager.activeSceneChanged<,>
+
+
+        SceneManager.MergeScenes(NewScene, SceneManager.GetActiveScene());
+
+        // Resources.UnloadUnusedAssets();
+        // GC.Collect();
+        
     }
 
     void Update()
