a38e34adee3e97f9680df6582f0f93140f308daf
김인수 <>
2024-07-04 13:46:25 -0700

    ghfg

--- Assets/script/DataManager.cs
+++ Assets/script/DataManager.cs
@@ -12,11 +12,7 @@ public class DataManager : MonoBehaviour
 
     public void SaveGame()
     {
-        SaveData ad = new SaveData(3);
-        ad.SceneNumber.ComponentSet = new string[]{"asd","??????","0.12" };
-        ad.level = 1;
-        ad.name = "obbj1";
-        Debug.Log(JsonUtility.ToJson(ad));
+        
 
     }
 
